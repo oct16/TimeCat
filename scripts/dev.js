6aa9d4e0d1da71c08e53f1ed1b906d693e902723
FengFan <mail@fengfan.me>
2020-03-12 11:45:45 +0800

    build: update dev config

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,17 +1,8 @@
 const execa = require('execa')
-const args = require('minimist')(process.argv.slice(2))
-const formats = args.formats || args.f
-const commit = execa.sync('git', ['rev-parse', 'HEAD']).stdout.slice(0, 7)
 
 execa(
     'rollup',
-    [
-        '-c',
-        'rollup.config.dev.js',
-        '-w',
-        '--environment',
-        [`COMMIT:${commit}`, `FORMATS:${formats || 'global'}`, 'SOURCE_MAP:true'].filter(Boolean).join(',')
-    ],
+    ['-c', 'rollup.config.dev.js', '-w', '--environment', ['FORMATS:umd', 'SOURCE_MAP:true'].filter(Boolean).join(',')],
     {
         stdio: 'inherit'
     }
