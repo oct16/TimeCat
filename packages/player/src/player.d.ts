76f85434fab30f9a8aff7a90ed1848eae8f3dfab
FengFan <mail@fengfan.me>
2020-03-17 21:32:56 +0800

    release: test version

--- packages/player/src/player.d.ts
+++ packages/player/src/player.d.ts
@@ -5,7 +5,6 @@ import { ProgressComponent } from './progress';
 import { ContainerComponent } from './container';
 export declare class PlayerComponent {
     data: SnapshotData[];
-    delayTime: number;
     speed: number;
     index: number;
     frameIndex: number;
