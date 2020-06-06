896e1c07e2d9ea26cd3af4e653f8e8315caedbec
FengFan <mail@fengfan.me>
2020-06-07 02:24:33 +0800

    chore: rename to configs

--- scripts/build.js
+++ scripts/build.js
@@ -11,7 +11,7 @@ async function run() {
         'rollup',
         [
             '-c',
-            'builders/rollup.config.prod.js',
+            'configs/rollup.config.prod.js',
             '--environment',
             [`NODE_ENV:${env}`, 'formats:umd', 'SOURCE_MAP:true', 'PROD_ONLY:true', 'TYPES:true', 'LEAN:true']
                 .filter(Boolean)
--- scripts/dev.js
+++ scripts/dev.js
@@ -3,7 +3,7 @@ const env = 'development'
 
 execa(
     'rollup',
-    ['-c', 'builders/rollup.config.dev.js', '-w', '--environment', [`NODE_ENV:${env}`].filter(Boolean).join(',')],
+    ['-c', 'configs/rollup.config.dev.js', '-w', '--environment', [`NODE_ENV:${env}`].filter(Boolean).join(',')],
     {
         stdio: 'inherit'
     }
