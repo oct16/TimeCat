a6d35862ba1ca286185f10ebca5407c810039820
FengFan <mail@fengfan.me>
2020-06-15 05:56:33 +0800

    feat: add jest config

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    testEnvironment: 'jsdom',
+    preset: 'ts-jest',
+    roots: ['<rootDir>/test'],
+    rootDir: __dirname,
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest'
+    },
+    coverageDirectory: 'coverage',
+    coverageReporters: ['html', 'lcov', 'text'],
+    watchPathIgnorePatterns: ['/node_modules/', '/dist/', '/.git/'],
+    modulePaths: ['packages', 'test'],
+    testRegex: '(/unit/.*|(\\.|/)(test|spec))\\.tsx?$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+    moduleNameMapper: {
+        '^@TimeCat/(.*?)$': '<rootDir>/packages/$1/src',
+        'packages/(.*)$': '<rootDir>/packages/$1'
+    },
+    verbose: true
+}
