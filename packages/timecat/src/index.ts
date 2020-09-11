037276faf9b006305e1e3f9887165f606bb83e30
FengFan <mail@fengfan.me>
2020-09-11 20:49:38 +0800

    feat: update timecat export types

--- packages/timecat/src/index.ts
+++ packages/timecat/src/index.ts
@@ -1,10 +1,18 @@
 import { Recorder } from '@timecat/recorder'
 import { Player } from '@timecat/player'
 
+export {
+    RecordData,
+    RecordType,
+    RecorderOptions,
+    ReplayOptions,
+    ReplayData,
+    ReplayHead,
+    ReplayPack
+} from '@timecat/share'
+
 const version = '__VERSION__'
 export { version }
 
 export { Player, Recorder }
 export { exportReplay, radix64, classifyRecords } from '@timecat/utils'
-
-export * from '@timecat/share'
