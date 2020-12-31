b5758db957bafb6c6dfeeef989db1bfd6b2b115a
oct16 <mail@fengfan.me>
2020-12-31 20:03:44 +0800

    fix(player): init hooks at runtime

--- packages/recorder/src/pluginable.ts
+++ packages/recorder/src/pluginable.ts
@@ -2,41 +2,45 @@ import { SyncHook } from 'tapable'
 import { RecordOptions } from './recorder'
 import { logError } from '@timecat/utils'
 
-const defaultPlugins: RecorderPlugin[] = [] // todo
-
-const checkHookAvailable = () => {
-    try {
-        new SyncHook().call()
-        return true
-    } catch (error) {
-        logError(`Plugin hooks is not available in the current env, because ${error}`)
-    }
-}
-
-const DEFAULT_HOOKS = {
-    beforeRun: new SyncHook(),
-    run: new SyncHook(),
-    emit: new SyncHook(['data']),
-    end: new SyncHook()
-}
-const HOOKS = checkHookAvailable()
-    ? DEFAULT_HOOKS
-    : Object.keys(DEFAULT_HOOKS).reduce((obj, key) => {
-          return { ...obj, [key]: () => {} }
-      }, {} as { [key in keyof typeof DEFAULT_HOOKS]: any })
-
 export interface RecorderPlugin {
     apply(recorder: Pluginable): void
 }
 
+type IHOOK = Record<'beforeRun' | 'run' | 'emit' | 'end', SyncHook<any, any, any>>
+
 export class Pluginable {
+    protected hooks: IHOOK
+    defaultPlugins: RecorderPlugin[] = [] // todo
+
     constructor(options?: RecordOptions) {
         this.initPlugin(options)
+
+        const DEFAULT_HOOKS = {
+            beforeRun: new SyncHook(),
+            run: new SyncHook(),
+            emit: new SyncHook(['data']),
+            end: new SyncHook()
+        }
+
+        const HOOKS = this.checkHookAvailable()
+            ? DEFAULT_HOOKS
+            : Object.keys(DEFAULT_HOOKS).reduce((obj, key) => {
+                  return { ...obj, [key]: () => {} }
+              }, {} as { [key in keyof typeof DEFAULT_HOOKS]: any })
+
+        this.hooks = HOOKS
     }
 
-    protected hooks = HOOKS
+    public checkHookAvailable = () => {
+        try {
+            new SyncHook().call()
+            return true
+        } catch (error) {
+            logError(`Plugin hooks is not available in the current env, because ${error}`)
+        }
+    }
 
-    public plugin = (type: keyof typeof HOOKS, cb: (data: any) => void) => {
+    public plugin = (type: keyof IHOOK, cb: (data: any) => void) => {
         const name = this.hooks[type].constructor.name
         const method = /Async/.test(name) ? 'tapAsync' : 'tap'
         this.hooks[type][method](type, cb)
@@ -48,7 +52,7 @@ export class Pluginable {
 
     private initPlugin(options?: RecordOptions) {
         const { plugins } = options || {}
-        this.plugins.push(...defaultPlugins, ...(plugins || []))
+        this.plugins.push(...this.defaultPlugins, ...(plugins || []))
     }
 
     protected pluginsOnload() {
