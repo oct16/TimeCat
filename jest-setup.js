429a428e626f7b6435ddbfc84cbe5f348b0d3bc5
oct16 <mail@fengfan.me>
2021-03-08 14:36:11 +0800

    test: update jest setup

--- jest-setup.js
+++ jest-setup.js
@@ -1,1 +1,10 @@
 jest.spyOn(global.console, 'error').mockImplementation(() => jest.fn())
+require('fake-indexeddb/auto')
+
+if (!process.env.LISTENING_TO_UNHANDLED_REJECTION) {
+    process.on('unhandledRejection', reason => {
+        throw reason
+    })
+    // Avoid memory leak by adding too many listeners
+    process.env.LISTENING_TO_UNHANDLED_REJECTION = true
+}
