63682e2ba5758310e4d20e88c62da9ce4fd1386b
oct16 <mail@fengfan.me>
2021-03-10 13:44:09 +0800

    test: add recorder test case

---
+++ packages/recorder/__tests__/recorder.spec.ts
@@ -0,0 +1,42 @@
+import { Recorder, RecorderModule } from '@timecat/recorder'
+import { delay } from '@timecat/utils'
+
+describe('Testing Recorder', () => {
+    test('Recorder status', async done => {
+        const recorder = new Recorder()
+        expect(recorder.status).toBe('stop')
+        await delay(200)
+        expect(recorder.status).toBe('running')
+        recorder.destroy()
+        expect(recorder.status).toBe('stop')
+        done()
+
+        const module = new RecorderModule()
+        expect(module.status).toBe('stop')
+        setTimeout(() => {
+            expect(module.status).toBe('running')
+            module.destroy()
+            expect(module.status).toBe('stop')
+            done()
+        }, 200)
+    })
+
+    test('Recorder Listeners', async () => {
+        const recorder = new RecorderModule()
+        await delay(200)
+        expect(recorder['watchers'].length).toBeGreaterThan(0)
+        expect(recorder['watchersInstance'].size).toBeGreaterThan(0)
+        expect(recorder['destroyStore'].size).toBeGreaterThanOrEqual(0)
+        expect(recorder['listenStore'].size).toBeGreaterThan(0)
+        await recorder['stop']()
+        expect(recorder['listenStore'].size).toBeLessThanOrEqual(0)
+        expect(recorder['watchers'].length).toBeGreaterThan(0)
+        recorder['record']({})
+        await delay(200)
+        await recorder.destroy()
+        expect(recorder['watchers'].length).toBeGreaterThan(0)
+        expect(recorder['watchersInstance'].size).toBeGreaterThan(0)
+        expect(recorder['destroyStore'].size).toBe(0)
+        expect(recorder['listenStore'].size).toBe(0)
+    })
+})
